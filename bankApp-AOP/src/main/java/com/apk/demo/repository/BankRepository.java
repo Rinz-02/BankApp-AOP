7d9ae71071a5a3a4e0aaaa0415f9f051c62f301c
Rinz-02 <willhan11122@gmail.com>
2026-01-18 22:15:22 +0400

    added Query for login function

--- bankApp-AOP/src/main/java/com/apk/demo/repository/BankRepository.java
+++ bankApp-AOP/src/main/java/com/apk/demo/repository/BankRepository.java
@@ -1,9 +1,11 @@
 package com.apk.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.apk.demo.model.Bank;
 
 public interface BankRepository extends JpaRepository<Bank,Long>
 {
-
+	Optional <Bank> findByNameIgnoreCase(String name); 
 }
