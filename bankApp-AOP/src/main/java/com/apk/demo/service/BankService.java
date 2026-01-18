f7c3b9f3d2af41b1e409251ee4d83b8aae3c7c41
Rinz-02 <willhan11122@gmail.com>
2026-01-18 22:18:17 +0400

    make a few changes due to application needs

--- bankApp-AOP/src/main/java/com/apk/demo/service/BankService.java
+++ bankApp-AOP/src/main/java/com/apk/demo/service/BankService.java
@@ -1,5 +1,6 @@
 package com.apk.demo.service;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.apk.demo.repository.*;
 import java.util.List;
+import java.util.Optional;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -24,58 +26,91 @@ public class BankService
 	@Autowired
 	private TransferRepository transferRepo;
 	
-	public void deposite(Double amount,Long id)
+	public Bank deposite(BigDecimal amount,Long id)
 	{
 		Bank bank = bankRepo.findById(id)
 				.orElseThrow( () -> new RuntimeException("Account Not Found"));
 		
-		bank.setAmount(bank.getAmount()+ amount);
+		bank.setAmount(bank.getAmount().add(amount));
 		bankRepo.save(bank);
-		
-		saveTransaction(id, null, "Deposite", amount);
+
+		saveTransaction(null, id, "Deposite", amount);
+		return bank;
 	}
 	
-	public void withdraw(Double amount,Long id)
+	public Bank withdraw(BigDecimal amount,Long id)
 	{
 		Bank bank = bankRepo.findById(id)			
 				.orElseThrow( () -> new RuntimeException("AccountNotFound"));
-		bank.setAmount(bank.getAmount() + amount);
+		bank.setAmount(bank.getAmount().subtract(amount));
 		bankRepo.save(bank);
 		
 		saveTransaction(id, null, "Withdraw", amount);
+		return bank;
 	}
 	
-	public void transfer(long fromId, Long toId, Double amount)
+	public Bank transfer(Long fromId, Long toId, BigDecimal amount)
 	{
+		if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+	        throw new IllegalArgumentException("Transfer amount isn't valid");
+	    }
+		if (fromId.equals(toId)) {
+			throw new IllegalArgumentException("Cannont transfer to same Account!");
+		}
+		
 		Bank fromBank = bankRepo.findById(fromId)
 				.orElseThrow( () -> new RuntimeException("AccountNotFound"));
 		Bank toBank = bankRepo.findById(toId)
 				.orElseThrow( () -> new RuntimeException("AccountNotFound"));
 		
-		if(amount > fromBank.getAmount())
-		{
+		if(amount.compareTo(fromBank.getAmount()) > 0)
+		{         
 			throw new RuntimeException("Not Enough Balance");
 		}
 			
-		fromBank.setAmount(fromBank.getAmount() - amount);	
-		toBank.setAmount(toBank.getAmount() + amount);
+		fromBank.setAmount(fromBank.getAmount().subtract(amount));	
+		toBank.setAmount(toBank.getAmount().add(amount));
 	
 		bankRepo.save(fromBank);
 		bankRepo.save(toBank);
 		
 		saveTransaction(fromId, toId, "Transfer", amount);
+		return fromBank;
 	}
 	
-	private void saveTransaction(long fromId,Long toId, String type,Double amount)
+	private void saveTransaction(Long fromId,Long toId, String type,BigDecimal amount)
 	{
-		//Error at id null value argument //quick fix type cast for null value
+		
 		Transfer transfer = new Transfer(null, type, fromId, toId, amount, new Date(System.currentTimeMillis()));
 		transferRepo.save(transfer);
 	}
 	
-	public List<Transfer> getHistory()
+	public List<Bank> findAllBanks()
 	{
-		return transferRepo.findAll();
+		return bankRepo.findAll();
+	}
+	
+	public List<Transfer> getHistory(Long id)
+	{
+		return transferRepo.findByFromAccountOrToAccountOrderByDateDesc(id, id);
+	}
+	
+	public Bank findById(Long id)
+	{
+		return bankRepo.findById(id).get();
+	}
+	
+	public Bank login(String name)
+	{
+		return bankRepo.findByNameIgnoreCase(name)
+				.orElseThrow( () -> new RuntimeException("User Not Found"));
+	}
+	
+	public Bank createAccount(String name)
+	{
+		Bank bank = new Bank();
+		bank.setName(name);
+		return bankRepo.save(bank);
 	}
 	
 }
