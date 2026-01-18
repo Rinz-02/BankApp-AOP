3f4061de4b0439013bbbeec6b91e923d32b33ec4
Rinz-02 <willhan11122@gmail.com>
2026-01-18 22:12:31 +0400

    added query to filer and sort Transaction History

--- bankApp-AOP/src/main/java/com/apk/demo/repository/TransferRepository.java
+++ bankApp-AOP/src/main/java/com/apk/demo/repository/TransferRepository.java
@@ -1,10 +1,12 @@
 package com.apk.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.apk.demo.model.Transfer;
 
 
 public interface TransferRepository extends JpaRepository<Transfer,Long>
 {
-
+	List<Transfer> findByFromAccountOrToAccountOrderByDateDesc(Long fromAccount, Long toAccount);
 }
